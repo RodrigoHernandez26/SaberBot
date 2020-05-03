2b95e96ce25ee6765dc939e94ecc37647acbdd57
Lucas Módolo <lucasmg2001@gmail.com>
2020-05-03 04:21:03 -0300

    icon update

--- web/main.py
+++ web/main.py
@@ -6,9 +6,11 @@ import requests
 connect()
 app = Flask(__name__)
 
+BotName = "SaberBot"
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', BotName = BotName)
 
 @app.route('/login/')
 def login():
@@ -19,15 +21,11 @@ def invite():
     return redirect('https://discordapp.com/oauth2/authorize?client_id=705878925363904543&scope=bot&permissions=8')
 
 @app.route('/dashboard/guild/', defaults= {'guild_id': "0"})
-@app.route('/dashboard/guild/<guild_id>')
-def guild(guild_id):
+@app.route('/dashboard/guild/<guild_id>/<guild_name>')
+def guild(guild_id, guild_name):
     if guild_id == '0':
         return redirect('/login/')
-    return render_template('guild.html')
-
-@app.route('/dashboard/guild/<guild_id>/config')
-def config(guild_id):
-    return render_template('config.html')
+    return render_template('guild.html', BotName = BotName)
 
 @app.route('/status/')
 def status():
@@ -36,7 +34,7 @@ def status():
         data = mysql_command('select rand, disc from status_api where id = 1', True)
     except Exception:
         db = False
-    return render_template('status.html', random= data[0]['rand'], discord= data[0]['disc'], db = db)
+    return render_template('status.html', random= data[0]['rand'], discord= data[0]['disc'], db = db, BotName = BotName)
 
 @app.route('/dashboard/')
 def dashboard():
@@ -55,7 +53,7 @@ def dashboard():
 
 @app.route('/dashboard/home/')
 def dashHome():
-    return render_template('dashboard.html', user_info= user_info, user_guilds= user_guilds, avatar_url = avatar_url)
+    return render_template('dashboard.html', user_info= user_info, user_guilds= user_guilds, avatar_url = avatar_url, BotName = BotName)
 
 if (__name__ == "__main__"):
     app.run(debug=True)
